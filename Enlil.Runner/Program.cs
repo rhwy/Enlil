06aacf338f02faa352b373d8ef783cae458b1f23
rhwy <github@rui.fr>
2019-09-18 08:46:03 +0200

    added runner for next experiments

---
+++ Enlil.Runner/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Enlil.Runner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
